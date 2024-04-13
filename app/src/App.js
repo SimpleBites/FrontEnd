b04c552f55da3cbc32a3572d9b1943fbbf8f8169
Maikel <94843275+SAG3R-2002@users.noreply.github.com>
2024-04-13 16:59:33 +0200

    Dashboard page (begin)
    verander gemaakt in de app.js file zodat de footer en nav niet meekomen bij de admin page

--- app/src/App.js
+++ app/src/App.js
@@ -1,47 +1,68 @@
 import * as React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import Recipes from "./components/pages/Recipes"
+import Recipes from "./components/pages/Recipes";
 import Profile from "./components/pages/Profile";
 import Contact from "./components/pages/Contact";
 import Footer from './components/Footer';
 import Navbar from "./components/navbar";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/register";
-import Admin from "./components/adminNav";
-
-import "../src/AppTw.css";
+import Admin from "./components/admin/Dashboard";
 import Favorites from "./components/pages/favorites";
-import Submit from "./components/pages/submit"
+import Submit from "./components/pages/submit";
 import Submitted from "./components/pages/submitted";
 import Recipe from "./components/pages/recipe";
 
-
-
-// import '../tailwind.config'
+import "../src/AppTw.css";
 
 function App() {
-  
+  const location = useLocation();
+
+  const showNavbarAndFooterRoutes = [
+    "/Home",
+    "/About",
+    "/Recipes",
+    "/Contact",
+    "/Login",
+    "/Register",
+    "/Profile",
+    "/Favorites",
+    "/Submit",
+    "/Submitted",
+    "/Recipe"
+  ];
+
+  const showNavbarAndFooter = showNavbarAndFooterRoutes.includes(location.pathname);
+
   return (
     <div className="App">
-      <Navbar />
-     <Routes>
+      {showNavbarAndFooter && <Navbar />}
+      <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Recipes" element={<Recipes />} />
-        <Route path="/Contact" element={<Contact />} /> 
-        <Route path="/Login" element={<Login />} /> 
-        <Route path="/Register" element={<Register />} /> 
-        <Route path="/Profile" element={<Profile />} /> 
-        <Route path="/Favorites" element={<Favorites />} /> 
-        <Route path="/Submit" element={<Submit />} /> 
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Favorites" element={<Favorites />} />
+        <Route path="/Submit" element={<Submit />} />
         <Route path="/Submitted" element={<Submitted />} />
         <Route path="/Recipe" element={<Recipe />} />
-        <Route path="/Admin" element={<Admin />} />
-      </Routes> 
-      <Footer /> 
+        <Route path="/Admin" element={<AdminWithLayout />} />
+      </Routes>
+      {showNavbarAndFooter && <Footer />}
     </div>
+  );
+}
+
+function AdminWithLayout() {
+  return (
+    <>
+      <Admin />
+    </>
   );
 }
 
---
+++ app/src/components/admin/Dashboard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import CustomLink from '../CustomLink';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "../admin/admin.css"
+import { faComment, faMagnifyingGlass, faUser, faUtensils } from '@fortawesome/free-solid-svg-icons';
+
+export default function AdminNav() {
+     return (
+          <div className="Container-Nav-Admin">
+               <div className="flex justify-center">
+                    <div className="flex flex-col items-center" >
+                         <img src='/logo.png' className="logo" alt='hi' style={{ width: '170px', height: '170px', marginRight: '150px' }} />
+                         <p className="Title-Nav-Admin">Hello, Admin</p>
+                         <div className="flex flex-col items-start">
+                              <CustomLink to="/Home" className="link">
+                                   <div className="flex items-center">
+                                        <img src='/tachometer.png' className=" " style={{ width: '30px', height: '22px', marginBottom: '5px', marginLeft: "-2px", marginRight: "8px" }}/>
+                                        <p className='mb-2 Content-Nav-Admin'>Dashboard</p>
+                                   </div>
+                              </CustomLink>
+                              <CustomLink to="/Comments" className="link">
+                                   <div className="flex items-center">
+                                        <FontAwesomeIcon icon={faComment} className='admin-nav-icons' />
+                                        <p className='mb-2 Content-Nav-Admin'>Comments</p>
+                                   </div>
+                              </CustomLink>
+                              <CustomLink to="/Adminrecipes" className="link">
+                                   <div className="flex items-center">
+                                        <FontAwesomeIcon icon={faUtensils} className='admin-nav-icons' />
+                                        <p className='mb-2 Content-Nav-Admin'>Recipes</p>
+                                   </div>
+                              </CustomLink>
+                              <CustomLink to="/Users" className="link">
+                                   <div className="flex items-center">
+                                        <FontAwesomeIcon icon={faUser} className='admin-nav-icons' />
+                                        <p className='mb-2 Content-Nav-Admin'>Users</p>
+                                   </div>
+                              </CustomLink>
+                                   <div className="flex items-center">
+                                        <FontAwesomeIcon icon={faMagnifyingGlass} className='admin-nav-icons'/>
+                                        <input className='mb-2 Content-Nav-Admin2' placeholder='search' />
+                                   </div>
+
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
+}
