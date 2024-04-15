e13461ce4b8151811bfbf878a9eaca42c6cef6f3
Maikel <94843275+SAG3R-2002@users.noreply.github.com>
2024-04-15 11:31:36 +0200

    dashboard fixed
    ...

--- app/src/components/pages/admin/dashboard.js
+++ app/src/components/pages/admin/dashboard.js
@@ -3,34 +3,39 @@ import AdminNav from '../../adminNav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faComment, faUser } from '@fortawesome/free-solid-svg-icons';
 import '../admin/dashboard.css';
+import CustomLink from '../../CustomLink';
 
 export default function Dashboard() {
   return (
-    <div className="flex justify-start items-center h-screen">
+    <div className="flex">
       <AdminNav />
-      <div className="ml-10 flex">
-        <a href="/Admin/arecipes" className="dashboard-box">
-          <FontAwesomeIcon icon={faUtensils} className='icon' />
-          <div className="text">
-            <p>Total food recipes</p>
-            <p>5</p>
-          </div>
-        </a>
-        <a href="/admin/Comments" className="dashboard-box">
-          <FontAwesomeIcon icon={faComment} className='icon' />
-          <div className="text">
-            <p>All comments</p>
-            <p>8</p>
-          </div>
-        </a>
-        <a href="/admin/panel" className="dashboard-box">
-          <FontAwesomeIcon icon={faUser} className='icon' />
-          <div className="text">
-            <p>All users</p>
-            <p>20</p>
-          </div>
-        </a>
-      </div>
-    </div>
+        <div className="ml-10 flex ">
+        <div className='flex'>
+            <CustomLink to="/Admin/arecipes" className="dashboard-box">
+            <FontAwesomeIcon icon={faUtensils} className='icon' />
+            <div className="text">
+                <p>Total food recipes</p>
+                <p>5</p>
+            </div>
+            </CustomLink>
+            <CustomLink to="/admin/Comments" className="dashboard-box">
+            <FontAwesomeIcon icon={faComment} className='icon' />
+            <div className="text">
+                <p>All comments</p>
+                <p>8</p>
+            </div>
+            </CustomLink>
+            <CustomLink to="/admin/panel" className="dashboard-box">
+            <FontAwesomeIcon icon={faUser} className='icon' />
+            <div className="text">
+                <p>All users</p>
+                <p>20</p>
+            </div>
+            </CustomLink>
+        </div>
+
+        </div>
+        </div>
+       
   );
 }
