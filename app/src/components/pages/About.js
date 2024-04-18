81d1dfe5cc8fa869c513d52fcf042de04a128d29
mosa0 <m.alsagheer@student.alfa-college.nl>
2024-04-18 11:16:22 +0200

    mosa about

--- app/src/components/pages/About.js
+++ app/src/components/pages/About.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import image from "../../../app/public/pancakes.png"
 
 export default function About() {
-     return <div class="flex justify-center items-center h-screen">
-     <h1 class="text-3xl text-black">About</h1>
-   </div>
-   
-   
-   
+  return (
+    <div className="flex justify-between items-start h-screen">
+      {/* Left Section */}
+      <div className="flex flex-col justify-start items-start ml-8" style={{ marginLeft: "175px" }}>
+        <h1 className="text-2xl md:text-4xl lg:text-5xl text-left mb-14 " style={{ color: "#365486" }}>Lorem ipsum dolor sit amet</h1>
+        <p className="text-base md:text-lg lg:text-xl mb-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat mauris nunc congue nisi vitae suscipit tellus. Pharetra massa massa ultricies mi quis hendrerit. Vel eros donec ac odio tempor orci dapibus ultrices in.</p>
+      </div>
+
+      {/* Right Section */}
+      <div className="mr-8">
+        <img src="app\public\pancakes.png" alt="Image" className="w-470 h-356" />
+      </div>
+    </div>
+  );
 }
-
-
